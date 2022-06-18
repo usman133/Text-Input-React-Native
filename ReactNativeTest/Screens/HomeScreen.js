946c6d996044cf106a3329bb5882c2583227d3e7
usman133 <usmankiani26@gmail.com>
2022-06-18 22:42:04 +1200

    Initial code

---
+++ ReactNativeTest/Screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+
+  return (
+    <view>
+      <Text>Home Screen</Text>
+    </view>
+  )
+
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
---
+++ TestProject/Screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LoginScreen = () => {
+{
+  return (
+    <view>
+      <Text>Login Screen</Text>
+    </view>
+  )
+}
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
